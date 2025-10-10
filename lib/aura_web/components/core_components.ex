018f78faa9dfc9cd1f2c3dc91e75d44df41b6c77
Evangelista <evangelistajoaop@gmail.com>
2025-10-10 00:37:00 -0300

    fix: add 'hidden' to input type values in core_components.ex
    Updated the input type attribute to include 'hidden' as a valid option for form inputs. This change enhances the flexibility of the input component by allowing hidden fields to be rendered.

--- lib/aura_web/components/core_components.ex
+++ lib/aura_web/components/core_components.ex
@@ -156,7 +156,7 @@ defmodule AuraWeb.CoreComponents do
   attr :type, :string,
     default: "text",
     values: ~w(checkbox color date datetime-local email file month number password
-               search select tel text textarea time url week)
+               search select tel text textarea time url week hidden)
 
   attr :field, Phoenix.HTML.FormField,
     doc: "a form field struct retrieved from the form, for example: @form[:email]"
